dfc5a361fe969ae8fb0f6ce9346354b52969dd15
Mankich10 <mankich828@gmail.com>
2025-04-26 18:28:28 +0600

    Add files via upload

--- file.js
+++ file.js
@@ -1,2 +1,13 @@
 'use strict'
-
+const catalog = document.getElementById("catalog")
+fetch("./data/products.json").then( uploadProducts )
+function uploadProducts(data) {
+    data.json().then( getProducts )
+}
+function getProducts(data) {
+    for(let phoneName in data) {
+        const phoneData = data[phoneName]
+        const phoneCard = getProductCard(phoneName, phoneData)
+        catalog.append(phoneCard)
+    }
+}
